182f4f51af58fca2399f487f93b6b9076ab0fbd7
waza-kaunda <wazakaunda@gmail.com>
2018-08-04 15:43:35 +0200

    create the product entity

---
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/draftwebflux/Product.java
@@ -0,0 +1,18 @@
+package com.baeldung.reactive.draftwebflux;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	
+	private int id;
+	
+	private String name;
+
+	// Getters and Setters generated via lombok
+	
+}
